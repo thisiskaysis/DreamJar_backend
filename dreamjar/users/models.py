48d88bd1d8c223aa67f488b62912d07a60cbf3e0
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-22 21:12:58 +1000

    added initial Parent and Child models

--- dreamjar/users/models.py
+++ dreamjar/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Parent(AbstractUser):
+    
+    def __str__(self):
+        return self.username
+    
+class Child(models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='child_images/', null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"
