8c4e1bc30dfcd7fc8eda9eaedf92beace2493fc0
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-22 23:55:29 +1000

    updated users/views with Authentication

--- dreamjar/users/views.py
+++ dreamjar/users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -102,6 +101,7 @@ class CustomAuthToken(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
+        
         return Response({
             'token': token.key,
             'user_id': user.id,
