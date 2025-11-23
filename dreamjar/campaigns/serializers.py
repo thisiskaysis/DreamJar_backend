c082122020a185996a20da0bd287d9c8bcb6bb46
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-23 23:52:03 +1000

    updated campaign serializers - check comments with mentors

--- dreamjar/campaigns/serializers.py
+++ dreamjar/campaigns/serializers.py
@@ -5,21 +5,28 @@ from users.models import Child
 from campaigns.models import Campaign
 
 class CampaignSerializer(serializers.ModelSerializer):
+    # I forgot why I might need these
     child = serializers.ReadOnlyField(source='child.id')
+    child_name = serializers.ReadOnlyField(source='child.name')
+    
     class Meta:
         model = apps.get_model('campaigns.Campaign')
         fields = '__all__'
-        read_only_fields = ['child']
+        read_only_fields = ['child', 'date_created']
     
     def create(self, validated_data):
-        #Pass child_id through the view context
+        # Connect the campaign to the child from context
         child_id = self.context['child_id']
         child = Child.objects.get(id=child_id)
         campaign = Campaign.objects.create(child=child, **validated_data)
         return campaign
 
 class CampaignDetailSerializer(CampaignSerializer):
+    
+    # Include all donations for this campaign & total amount
+    # Do I put these here or views?
     donations = DonationSerializer(many=True, read_only=True)
+    total_donated = serializers.SerializerMethodField()
     
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
@@ -27,7 +34,8 @@ class CampaignDetailSerializer(CampaignSerializer):
         instance.goal = validated_data.get('goal', instance.goal)
         instance.image = validated_data.get('image', instance.image)
         instance.is_open = validated_data.get('is_open', instance.is_open)
-        instance.date_created = validated_data.get('date_created', instance.date_created)
-        instance.child = validated_data.get('child', instance.child)
         instance.save()
         return instance
+    
+    def get_total_donated(self, obj):
+        return sum(donation.amount for donation in obj.donations.all())
