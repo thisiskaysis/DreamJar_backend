c40b91236cda04a9437ad7e8a98200cf33401208
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-26 00:42:58 +1000

    updated campaign urls to nest campaigns under children - time to test

--- dreamjar/campaigns/urls.py
+++ dreamjar/campaigns/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('campaigns/', views.CampaignList.as_view()),
+    path('campaigns/', views.PublicCampaignList.as_view()),
     path('campaigns/<int:pk>/', views.CampaignDetail.as_view()),
+    path('children/<int:pk>/campaigns/', views.ChildCampaignList.as_view()),
 ]
