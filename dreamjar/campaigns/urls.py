13c88ac12ce10ff06b558b25b784b7681d1ce5eb
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-11-30 22:26:15 +1000

    updated all url endpoints

--- dreamjar/campaigns/urls.py
+++ dreamjar/campaigns/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('campaigns/', views.PublicCampaignList.as_view()),
-    path('campaigns/<int:pk>/', views.CampaignDetail.as_view()),
-    path('children/<int:pk>/campaigns/', views.ChildCampaignList.as_view()),
+    path('campaigns/', views.PublicCampaignList.as_view()), #Public - browse all
+    path('campaigns/<int:pk>/', views.CampaignDetail.as_view()), #Public - GET, Owner, PUT/DELETE
+    path('children/<int:pk>/campaigns/', views.ChildCampaignList.as_view()), #Parents manage child's campaigns here
 ]
--- dreamjar/donations/urls.py
+++ dreamjar/donations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('campaigns/<int:pk>/donations/', views.CampaignDonationList.as_view()),
+    path('campaigns/<int:pk>/donations/', views.CampaignDonationList.as_view()), #Public - donate here
+    path('donations/', views.DonationList.as_view()), #Auth required
 ]
