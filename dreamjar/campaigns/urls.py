c886d5d42f014ee3eaa514bb3f5d7141e683d799
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-02-12 15:52:17 +1000

    little note to remember what I've done

--- dreamjar/campaigns/urls.py
+++ dreamjar/campaigns/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('campaigns/', views.PublicCampaignList.as_view()), #Public - browse all
     path('campaigns/<int:pk>/', views.CampaignDetail.as_view()), #Public - GET, Owner, PUT/DELETE
     path('children/<int:pk>/campaigns/', views.ChildCampaignList.as_view()),
-    path('accounts/profile/', views.PublicCampaignList.as_view()), #Parents manage child's campaigns here
+    path('accounts/profile/', views.PublicCampaignList.as_view()), #THIS IS A HACK - we want to redirect to the public campaign list, but we need to have a URL that matches the default login redirect URL for Google
 ]
